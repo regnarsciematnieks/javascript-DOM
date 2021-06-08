7691fc73ae260d949327bc1e88285d4d38220fb6
regnarsciematnieks <regnarsc@inbox.lv>
2021-06-08 16:00:55 +0300

    ceturtais darbs darits

--- 04-toggle-many-elements/global.js
+++ 04-toggle-many-elements/global.js
@@ -1,1 +1,15 @@
 // Your JavaScript goes here.
+window.addEventListener("load", function(){
+    const button = document.getElementById('button')
+
+    button.addEventListener('click', function(e){
+        e.preventDefault()
+        document.querySelectorAll('.toggle_me').forEach(function(e) {
+            if(e.style.display == 'none'){
+                e.style.display = 'block'
+            } else {
+                e.style.display = 'none'
+            }
+        })
+    })
+})
