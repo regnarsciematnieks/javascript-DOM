4b8d09e380776d3243b809b3afbf188ecd0f5c0b
regnarsciematnieks <regnarsc@inbox.lv>
2021-06-08 16:10:52 +0300

    piektais darbs darits

--- 05-toggle-elements-of-dom-path/QUESTIONS.md
+++ 05-toggle-elements-of-dom-path/QUESTIONS.md
@@ -6,4 +6,4 @@
 
 > Describe the contract you used for finding the movies to toggle in the DOM. How is this function different from other functions that find elements in the DOM?
 
-Your reply here...
+Your reply here... Lai atrastu elementu ar second_five klasi un visus elementus zem tā, es izmantoju querySelectorAll ()
--- 05-toggle-elements-of-dom-path/global.js
+++ 05-toggle-elements-of-dom-path/global.js
@@ -1,1 +1,14 @@
 // Your JavaScript goes here.
+window.addEventListener('load', function(){
+    const button = document.getElementById("toggle_button")
+
+    button.addEventListener('click', function(){
+        document.querySelectorAll('.second_five li').forEach((e) => {
+            if(e.style.display == 'block') {
+                e.style.display = 'none'
+            } else {
+                e.style.display = 'block';
+            }
+        });
+    });
+});
